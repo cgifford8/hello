ba8f8d3fada13133a603c24775a1988e0851989e
Charles <cgifford8@huskers.unl.edu>
2021-08-26 13:18:06 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Charles Gifford
+ *         cgifford8@huskers.unl.edu
+ * Date: 2020/08/26
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Charles Gifford\n");
+
+  return 0;
+}
